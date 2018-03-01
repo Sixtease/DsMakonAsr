476eedabdf3a7d707b72a5a30f51112f034ddfef
Jan Oldrich Kruza <sixtease+github@gmail.com>
2018-03-01 12:14:46 +0100

    import tweaks

--- bin/import.sh
+++ bin/import.sh
@@ -3,13 +3,13 @@
 # RUN THIS FIRST:
 # . config.sh
 
-bin/import/get_humanic_subs.pl $SUBS_MASK > data/humanic_subs.txt
+#bin/import/get_humanic_subs.pl $SUBS_MASK > data/humanic_subs.txt
 bin/import/subs2train.pl < data/humanic_subs.txt "$AUDIO_DIR" data data/temp-train.csv data/wide-test.csv data/wide-corpus.txt
 awk -f bin/import/split-train-dev.awk data/temp-train.csv
 
 bin/import/asciize.pl < data/wide-train.csv > data/train.csv
 bin/import/asciize.pl < data/wide-dev.csv > data/dev.csv
 bin/import/asciize.pl < data/wide-test.csv > data/test.csv
-bin/import/asciize.pl < data/wide-corpus.csv > data/corpus.csv
+bin/import/asciize.pl < data/wide-corpus.txt > data/corpus.txt
 
 rm data/temp-train.csv
--- bin/importlm.pl
+++ bin/importlm.pl
@@ -8,16 +8,22 @@ use open qw(:std :utf8);
 use Asciize qw(asciize);
 
 my ($in_lm_fn, $out_vocab_fn) = @ARGV;
+(my $wide_out_vocab_fn = $out_vocab_fn) =~ s{/([^/]+)$}{/wide-$1};
 open my $in_lm_fh, '<:encoding(iso-8859-2)', $in_lm_fn or die "Couldn't open language model '$in_lm_fn': $!";
 open my $out_vocab_fh, '>:utf8', $out_vocab_fn or die "Couldn't open output vocabulary file '$out_vocab_fn': $!";
+open my $wide_out_vocab_fh, '>:utf8', $wide_out_vocab_fn or die "Couldn't open wide output vocabulary file '$wide_out_vocab_fn': $!";
 
 while (<$in_lm_fh>) {
     my $normalized = lc;
+    $normalized =~ s/!!unk/<unk>/g;
     asciize($normalized);
     print $normalized;
     if (/\\1-grams:/ .. /^$/) {
-        if ($normalized =~ /^\S+\s+(\S+)/) {
-            print {$out_vocab_fh} lc($1), "\n";
+        if (/^\S+\s+(\S+)/) {
+            my $lc = lc($1);
+            print {$wide_out_vocab_fh} "$lc\n";
+            asciize($lc);
+            print {$out_vocab_fh} "$lc\n";
         }
     }
 }
